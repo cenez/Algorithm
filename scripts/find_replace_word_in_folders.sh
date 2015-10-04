67139fc6f137e721b7949c4f8f14ac403af5fade
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2015-10-03 23:54:28 -0300

    $

--- scripts/find_replace_word_in_folders.sh
+++ scripts/find_replace_word_in_folders.sh
@@ -2,16 +2,9 @@
 P1=""
 P2="\/\/"
 
-OLD1=$P1"System.Console.Write"
-NEW1=$P2"System.Console.Write"
+OLD1=$P1"Old Word"
+NEW1=$P2"New Word"
 find . -iname *.cs | xargs sed -i 's/'"$OLD1"'/'"$NEW1"'/g'
 
-OLD2=$P1"Console.Write"
-NEW2=$P2"Console.Write"
-find . -iname *.cs | xargs sed -i 's/'"$OLD2"'/'"$NEW2"'/g'
-
-OLD3=$P1"Trace.Write"
-NEW3=$P2"Trace.Write"
-find . -iname *.cs | xargs sed -i 's/'"$OLD3"'/'"$NEW3"'/g'
 
 
