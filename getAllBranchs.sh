f4aa5893945d6fcb8d840916d289e27917bf3cf3
Cenez Araujo de Rezende <cenezaraujo@gmail.com>
2017-04-19 14:09:48 -0300

    getAllBranchs.sh

--- getAllBranchs.sh
+++ getAllBranchs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master `; do
    git branch --track ${branch#remotes/origin/} $branch
+   echo "Command: git branch --track ${branch#remotes/origin/} $branch"
 done
 
